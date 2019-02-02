c5ab3c88701ac12e69b137de3c91e4bef10ad6dd
Blanchot <Blanchot@users.noreply.github.com>
2019-02-02 11:42:49 +0100

    Commit

--- cronTest.py
+++ cronTest.py
@@ -1,1 +1,16 @@
-#cronTest.py
+#!/usr/bin/python3
+# cronTest.py
+# Python script test for crontab
+# NOTE: First create cronTest.txt by hand
+# Crontab instruction: 
+# 
+
+import time
+
+localtime = time.asctime(time.localtime(time.time()))
+
+fileobj= open('cronTest.txt', 'a')
+fileobj.write(localtime)
+fileobj.close
+
+print("Local current time :", localtime)
