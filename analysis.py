993bc1452128605ebec6149f0a88ec25b952cc8c
Blanchot <Blanchot@users.noreply.github.com>
2019-02-07 10:13:26 +0100

    Testing...

--- analysis.py
+++ analysis.py
@@ -9,18 +9,27 @@ with f:
   reader = csv.DictReader(f)
   total_stake= 0
   num_days= 0
-  reversed_list= []
+  records= []
   for row in reader:
     num_days= num_days +1
     day_stake= float(row['stake'])
     total_stake= total_stake + day_stake
+    records= records.append(row)
     #total_stake= round(total_stake,8)
     #print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
 
 print('\nNumber of days:', num_days)
 total_stake= round(total_stake,8)
 print('Total stakes:', total_stake)
-print('Daily average:', total_stake/num_days)
+daily_average= round(total_stake/num_days,8)
+print('Daily average:', daily_average)
+
+print()
+recordsReversed= list(reversed(records))
+
+for row in recordsReversed:
+  print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
+
 
 '''
   for row in reader:
