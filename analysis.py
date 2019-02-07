6ff6e6a7679c9641e899dba3ff82a5116563d55f
Blanchot <Blanchot@users.noreply.github.com>
2019-02-07 11:50:07 +0100

    Added 'Expected (interest) per last balance'

--- analysis.py
+++ analysis.py
@@ -19,21 +19,22 @@ with f:
     #total_stake= round(total_stake,8)
     #print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
 
+
+dayListReversed= list(reversed(dayList))
+
 print('\nNumber of days:', num_days)
 total_stake= round(total_stake,8)
 print('Total stakes:', total_stake)
 daily_average= round(total_stake/num_days,8)
 print('Daily average:', daily_average)
+last_balance= float(dayListReversed[0]["balance"])
+expected= (last_balance/10)/365
+expected= round(expected,8)
+print('Expected (per last balance):', expected)
 
 print()
-dayListReversed= list(reversed(dayList))
-
 for row in dayListReversed:
   print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
 
 
-'''
-  for row in reader:
-    print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
-'''
 
