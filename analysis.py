598a02656dc8185378f94f7758f47966c782e091
Blanchot <Blanchot@users.noreply.github.com>
2019-02-07 07:35:07 +0100

    Bug fixes

--- analysis.py
+++ analysis.py
@@ -14,13 +14,15 @@ with f:
     day_stake= float(row['stake'])
     total_stake= total_stake + day_stake
     total_stake= round(total_stake,8)
-    print(total_stake)
+    print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
 
   print('\nNumber of days:', num_days)
   print('Total stakes:', total_stake)
   print('Daily average:', total_stake/num_days)
-  
+
+
+'''
   for row in reader:
     print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
-
+'''
 
