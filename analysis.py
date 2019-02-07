bdf946470f358f76376289ada3917020c9728271
Blanchot <Blanchot@users.noreply.github.com>
2019-02-07 10:06:17 +0100

    Testing

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
-# Analysis of BlockExplorer_1.py results
+# Simple analysis of BlockExplorer_1.py results
+# Fields are: date,time,timestamp,balance,stake
 
 import csv
-# date, time, timestamp, balance, stake
 
 f = open('account.txt', 'r')
 
@@ -9,17 +9,18 @@ with f:
   reader = csv.DictReader(f)
   total_stake= 0
   num_days= 0
+  reversed_list= []
   for row in reader:
     num_days= num_days +1
     day_stake= float(row['stake'])
     total_stake= total_stake + day_stake
-    total_stake= round(total_stake,8)
-    print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
+    #total_stake= round(total_stake,8)
+    #print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
 
-  print('\nNumber of days:', num_days)
-  print('Total stakes:', total_stake)
-  print('Daily average:', total_stake/num_days)
-
+print('\nNumber of days:', num_days)
+total_stake= round(total_stake,8)
+print('Total stakes:', total_stake)
+print('Daily average:', total_stake/num_days)
 
 '''
   for row in reader:
