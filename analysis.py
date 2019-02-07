8f16c7a95382a6052bd7924a2c7cecb64a53884b
Blanchot <Blanchot@users.noreply.github.com>
2019-02-07 07:24:25 +0100

    Test

--- analysis.py
+++ analysis.py
@@ -1,1 +1,29 @@
+# Analysis of BlockExplorer_1.py results
+
+import csv
+# date, time, timestamp, balance, stake
+
+f = open('account.txt', 'r')
+
+with f:
+  reader = csv.DictReader(f)
+  total_stake= 0
+  num_days= 0
+  for row in reader:
+    num_days= num_days +1
+    day_stake= float(row['stake'])
+    total_stake= total_stake + day_stake
+    total_stake= round(total_stake,8)
+    print(total_stake)
+
+print('\nNumber of days:', num_days)
+print('Total stakes:', total_stake)
+print('Daily average:', total_stake/num_days)
+
+
+with f:
+  reader = csv.DictReader(f)
+  for row in reader:
+    print(row['date'], row['time'], row['timestamp'], row['balance'], row['stake'])
+
 
